2b498fce23f4b03e8f2cb2da6388eb6835f49e1b
Alexander Sagen <alexmsagen@gmail.com>
2019-05-03 14:08:33 +0200

    Removed test function to generate graph

--- src/main/java/app/sagen/serverflight/WorldController.java
+++ src/main/java/app/sagen/serverflight/WorldController.java
@@ -144,45 +144,6 @@ public class WorldController {
         getAllGraphs().forEach(FlightGraph::shutdown);
     }
 
-    public void createTestGrid() {
-        for (FlightGraph flightGraph : getAllGraphs()) {
-            Graph graph = new Graph();
-
-            Vertex middle = new Vertex("Middle", 0.5f, 125.5f, 0.5f, false);
-            Vertex northDown = new Vertex("North-Down", 50.5f, 100.5f, 0.5f, true);
-            Vertex southDown = new Vertex("South-Down", -50.5f, 100.5f, 0.5f, true);
-            Vertex eastDown = new Vertex("East-Down", 0.5f, 100.5f, 50.5f, true);
-            Vertex westDown = new Vertex("West-Down", 0.5f, 100.5f, -50.5f, true);
-            Vertex northWestDown = new Vertex("NorthWest-Down", 50.5f, 100.5f, -50.5f, true);
-
-            graph.addVertex(middle);
-            graph.addVertex(northDown);
-            graph.addVertex(southDown);
-            graph.addVertex(eastDown);
-            graph.addVertex(westDown);
-            graph.addVertex(northWestDown);
-
-            graph.addEdge(middle, northDown);
-            graph.addEdge(middle, southDown);
-            graph.addEdge(middle, eastDown);
-            graph.addEdge(middle, westDown);
-            graph.addEdge(northDown, northWestDown);
-            graph.addEdge(westDown, northWestDown);
-            graph.addEdge(middle, northWestDown);
-
-            // set grid and recalculate
-            flightGraph.setGraph(graph);
-            flightGraph.setupFlightMovers();
-
-            // place beacons
-            World world = Bukkit.getWorld(flightGraph.getWorld());
-            world.getBlockAt((int) northDown.getX() - 1, (int) northDown.getY(), (int) northDown.getZ()).setType(Material.BEACON);
-            world.getBlockAt((int) southDown.getX() - 1, (int) southDown.getY(), (int) southDown.getZ()).setType(Material.BEACON);
-            world.getBlockAt((int) eastDown.getX() - 1, (int) eastDown.getY(), (int) eastDown.getZ()).setType(Material.BEACON);
-            world.getBlockAt((int) westDown.getX() - 1, (int) westDown.getY(), (int) westDown.getZ()).setType(Material.BEACON);
-        }
-    }
-
     public void playerJoin(Player player) {
         if(isAdminmode(player)) {
             adminmmodeBossbar.addPlayer(player);
